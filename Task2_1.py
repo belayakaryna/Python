91c8d683f997c97c9b1ec956fc554eb884f232ec
belayakaryna <69214223+belayakaryna@users.noreply.github.com>
2020-08-11 14:04:18 +0300

    Add files via upload

--- Task2_1.py
+++ Task2_1.py
@@ -3,10 +3,12 @@ def squares(a, b, n):
   if a == b:
        return a, b, n+1
   elif a < b:
+       print('Отрезаем квадрат со сторонами: '+str(a), str(a))
        return squares(a, b-a, n+1)
   else:
+       print('Отрезаем квадрат со сторонами: '+str(b), str(b))
        return squares(a-b, b, n+1)
 a = int(input('Введите сторону a \n'))
 b = int(input('Введите сторону b \n'))
-print('Длины рёбер квадрата и количество получаемых квадратов '+str(squares(a, b, n)))
+print('Размер  сторон последнего квадрата и  количество получаемых квадратов '+str(squares(a, b, n)))
 
